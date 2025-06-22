2f846b8aa76cddeb8dad2796c1da75047992a8cc
Padula, Diego A. <96376640+paduladiego@users.noreply.github.com>
2025-06-22 16:21:19 +0000

    Aula_Dia4-4d4

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+          <h1>Welcome to My Next.js App</h1>
+          <h2>Editado pelo Paduladiego</h2>
+      <p>This is the home page.</p>
+    </div>
+  );
+}   
+export default Home;
