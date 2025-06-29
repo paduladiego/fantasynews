0efbfcc79373d8c5b51894867f93e84385dea2ea
Padula, Diego A. <96376640+paduladiego@users.noreply.github.com>
2025-06-29 22:35:59 +0000

    insere frase nova de teste

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ function Home() {
           <h1>Welcome to My Next.js App</h1>
           <h2>Editado pelo Paduladiego</h2>
       <p>This is the home page.</p>
+      <p>teste de mais uma linha.</p>
     </div>
   );
 }   
