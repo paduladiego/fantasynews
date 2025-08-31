b8ee85b0fad98567a7f41ca81946f28526812cdc
Padula, Diego A. <96376640+paduladiego@users.noreply.github.com>
2025-08-31 06:00:13 +0000

    update files

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 function Home() {
   return (
     <div>
-      <h1>Welcome to My Next.js App</h1>
-      <h2>Editado pelo Paduladiego</h2>
+      <h1>Welcome to FantasyNews by DESIRE Studio</h1>
+      <h2>
+        Desenvolvido por <a href="https://padula.one">Padula.one</a>
+      </h2>
       <p>This is the home page.</p>
       <p>teste de mais uma linha.</p>
     </div>
